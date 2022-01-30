76fea8eaf3ad78c8643e1bb5495a049f2794e4c6
Valera777 <valeriy.g@svitsoft.com>
2022-01-30 13:02:50 +0200

    fix port

--- src/http/controller-one.ts
+++ src/http/controller-one.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('food')
 export class FoodController {
   @Get()
   find(): string {
     return 'New controller for foods';
+  }
+  @Post()
+  create(): string {
+    return 'Instance is created';
   }
 }
