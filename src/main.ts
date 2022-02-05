c3672fe605efeaa4ee9e079aa82ea39a68766e78
Valera777 <valeriy.g@svitsoft.com>
2022-02-05 14:20:56 +0200

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('++++++++++++++++++++++++++++');
-  console.log(process.env);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
